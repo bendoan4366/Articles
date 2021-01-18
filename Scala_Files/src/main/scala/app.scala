a5aeff72960004ad669ee5deccd8d21954506470
bendoan4366 <ben.doan4366@gmail.com>
2021-01-17 19:02:15 -0500

    adding initial transformations for intro article

--- Scala_Files/src/main/scala/app.scala
+++ Scala_Files/src/main/scala/app.scala
@@ -1,6 +1,19 @@
+import org.apache.spark.sql.SparkSession
+
 object app {
 
   def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession
+      .builder()
+      .appName("spark-example")
+      .master("local")
+      .getOrCreate()
+
+    val df = spark.read.option("header", true).option("inferSchema", true).csv("data/test_data.csv")
+    df.show(10)
+
+    df.printSchema()
 
   }
 
